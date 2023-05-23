61a224454bfb03ec179cc2e701e05379de79a8e4
AlexSnider <alexsnider8@gmail.com>
2023-05-23 15:51:14 -0300

    1.1.0

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,41 @@
 import { useState } from 'react'
+import './App.css'
+import Todo from './components/todo';
+import TodoForm from './components/TodoForm';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'Criar funcionalidade x no sistema',
+      category: 'Trabalho',
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: 'Estudar react',
+      category: 'Estudo',
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: 'Criar funcionalidade x no github',
+      category: 'Trabalho',
+      isCompleted: false,
+    },
+  ]);
 
   return (
-    <div>
-      olá
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <Todo todo={todo} />
+        ))}
+      </div>
+      <TodoForm />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
