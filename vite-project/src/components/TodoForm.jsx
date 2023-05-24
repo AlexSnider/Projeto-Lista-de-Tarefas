53d46e7ad5449eecb773bb37369de2b3c5b47855
AlexSnider <alexsnider8@gmail.com>
2023-05-24 09:56:33 -0300

    1.0.0

--- vite-project/src/components/TodoForm.jsx
+++ vite-project/src/components/TodoForm.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import { useState } from 'react'
 
 const TodoForm = () => {
+
+  const [value, setValue] = useState('')
+  const [category, setCategory] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!value || !category) return
+    setValue('')
+    setCategory('')
+  }
+
   return (
     <div className='todo-form'>
       <h2>Criar Tarefa:</h2>
-      <form>
-        <input type="text" placeholder='Digite o Título' />
-        <select>
+      <form onSubmit={handleSubmit}>
+        <input type="text"
+          placeholder='Digite o Título'
+          value={value}
+          onChange={(e) => setValue(e.target.value)} />
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
           <option value="">Selecione uma Categoria</option>
           <option value="Trabalho">Trabalho</option>
           <option value="Pessoal">Pessoal</option>
