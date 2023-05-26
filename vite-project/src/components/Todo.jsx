b5bd1a18e46a3dba105b9469455a431ead6cfd90
AlexSnider <alexsnider8@gmail.com>
2023-05-26 11:14:00 -0300

    Version 1.0.0 - Stable

--- vite-project/src/components/Todo.jsx
+++ vite-project/src/components/Todo.jsx
@@ -1,18 +1,18 @@
 import { useState } from 'react'
 
-const Todo = ({ todo, deleteTodo }) => {
+const Todo = ({ todo, deleteTodo, completeTodo }) => {
   return (
-    <div className="todo">
+    <div className="todo" style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
       <div className="content">
         <p>{todo.text}</p>
         <p className="category">({todo.category})</p>
       </div>
       <div>
-        <button className='complete'>Completar</button>
+        <button className='complete' onClick={() => completeTodo(todo.id)}>Completar</button>
         <button className='delete' onClick={() => deleteTodo(todo.id)}>Deletar</button>
       </div>
     </div>
   )
 }
 
-export default Todo
+export default Todo;
